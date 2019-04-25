299e5269aa9ce2991c2602aa83ece72bd5140868
Meet Janani <meetjanani47@gmail.com>
2019-04-25 23:26:45 +0530

    Added Test Controller & WaitForSeconds Service.

---
+++ CouponManager.Api/Services/WaitForSeconds.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace CouponManager.Api.Services
+{
+    public class WaitForSeconds
+    {
+        public async Task WaitSeconds(int milliseconds)
+        {
+            await Task.Delay(milliseconds);
+        }
+    }
+}
