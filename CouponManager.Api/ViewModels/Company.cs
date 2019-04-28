f705547f254c9640e9f18b4f1ea57b3b105749d4
Meet Janani <meetjanani47@gmail.com>
2019-04-28 21:46:09 +0530

    Database Design : Modal Added

---
+++ CouponManager.Api/ViewModels/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CouponManager.Api.ViewModels
+{
+    public class Company
+    {
+        [Required]
+        public int Company_ID { get; set; }
+
+        [Required]
+        public String Company_Name { get; set; }
+
+        [Required]
+        public String Company_Url { get; set; }
+
+        [Required]
+        public String CompanyEmail { get; set; }
+
+        [Required]
+        public String UserMame { get; set; }
+
+
+
+    }
+}
