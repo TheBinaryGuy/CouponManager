28566bdfaa4a335e9fa4e78592bbfac92df98e15
TheBinaryGuy <heet_mehta@hotmail.com>
2019-04-30 20:16:44 +0530

    Added couponservice and added logic to add the user to specific roles.

--- CouponManager.Api/Controllers/CouponsController.cs
+++ CouponManager.Api/Controllers/CouponsController.cs
@@ -1,7 +1,6 @@
 ﻿using CouponManager.Api.Data;
-using CouponManager.Api.Models;
+using CouponManager.Api.Services;
 using CouponManager.Api.ViewModels;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,17 +13,18 @@ namespace CouponManager.Api.Controllers
     [Produces("application/json")]
     [Route("cm/api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Authorize]
     public class CouponsController : ControllerBase
     {
         private readonly AppDbContext _context;
-
+        private readonly ICouponService _couponService;
         private readonly CancellationTokenSource _tokenSource;
         private readonly CancellationToken _cancellationToken;
 
-        public CouponsController(AppDbContext context)
+        public CouponsController(AppDbContext context, ICouponService couponService)
         {
             _context = context;
+            _couponService = couponService;
 
             _tokenSource = new CancellationTokenSource();
             _cancellationToken = _tokenSource.Token;
@@ -33,34 +33,20 @@ namespace CouponManager.Api.Controllers
         [HttpGet("{limit?}/{offset?}")]
         public async Task<IActionResult> GetAsync(int limit = 50, int offset = 0)
         {
-            var coupons = await _context.Coupons.Skip(limit * offset).Take(limit).AsNoTracking().ToListAsync(_cancellationToken);
-            return new JsonResult(coupons);
+            return new JsonResult(await _couponService.GetAsync(limit, offset).ToListAsync(_cancellationToken));
         }
 
         [HttpGet("{couponName}")]
         public async Task<IActionResult> GetAsync(string couponName)
         {
-            var coupon = await _context.Coupons.FirstOrDefaultAsync(c => c.Name == couponName, _cancellationToken);
-            return new JsonResult(coupon);
+            return new JsonResult(await _couponService.GetAsync(couponName, _cancellationToken));
         }
 
         [HttpPost]
         public async Task<IActionResult> PostAsync(CouponViewModel model)
         {
             var userId = User.Claims.Where(c => c.Type == "UserId").Select(c => c.Value).SingleOrDefault();
-            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == model.CategoryName);
-            var domain = await _context.Domains.FirstOrDefaultAsync(c => c.Name == model.DomainName && c.UserId == userId);
-            await _context.AddAsync(
-                new Coupon
-                {
-                    Name = model.Name,
-                    Code = model.Code,
-                    CategoryId = category.Id,
-                    DomainId = domain.Id,
-                    UserId = userId
-                }
-            );
-            await _context.SaveChangesAsync(_cancellationToken);
+            await _couponService.AddAsync(model, userId, _cancellationToken);
             return NoContent();
         }
     }
---
+++ CouponManager.Api/Services/CouponService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using CouponManager.Api.Data;
+using CouponManager.Api.Models;
+using CouponManager.Api.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace CouponManager.Api.Services
+{
+    public class CouponService : ICouponService
+    {
+        private readonly AppDbContext _context;
+
+        public CouponService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<Coupon> GetAsync(int limit, int offset)
+        {
+            return _context.Coupons.Skip(limit * offset).Take(limit).AsNoTracking();
+        }
+
+        public async Task<Coupon> GetAsync(string couponName, CancellationToken _cancellationToken = default)
+        {
+            return await _context.Coupons.FirstOrDefaultAsync(c => c.Name == couponName, _cancellationToken);
+        }
+
+        public async Task AddAsync(CouponViewModel model, string userId, CancellationToken _cancellationToken = default)
+        {
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == model.CategoryName);
+            var domain = await _context.Domains.FirstOrDefaultAsync(c => c.Name == model.DomainName && c.UserId == userId);
+            await _context.AddAsync(
+                new Coupon
+                {
+                    Name = model.Name,
+                    Code = model.Code,
+                    CategoryId = category.Id,
+                    DomainId = domain.Id,
+                    UserId = userId
+                }
+            );
+            await _context.SaveChangesAsync(_cancellationToken);
+        }
+    }
+}
